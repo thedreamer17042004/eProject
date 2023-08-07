9c79869b9ab222fc498e617b79ec0342d9ce0612
Nam Văn <nguyenvannam@gmail.com>
2023-08-07 17:53:24 +0700

    about

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+
+
+// about us
+$('.my-hot-product').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+});
